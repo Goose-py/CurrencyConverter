54ba64c71b28696020f405f56e02099a318cafa2
Goose-py <slama.yassine3@gmail.com>
2024-05-22 19:10:38 +0200

    Added currency list

--- app/src/main/java/com/example/currencyconverter/CurrencyListActivity.kt
+++ app/src/main/java/com/example/currencyconverter/CurrencyListActivity.kt
@@ -1,20 +1,25 @@
 package com.example.currencyconverter
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.activity.ComponentActivity
 
-class CurrencyListActivity : AppCompatActivity() {
+class CurrencyListActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_currency_list)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val listView = findViewById<ListView>(R.id.listView)
+
+        val exchangeRateDatabaseObj = ExchangeRateDatabase()
+
+        val adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1,
+            exchangeRateDatabaseObj.currencies
+        )
+        listView.adapter = adapter
+
     }
 }
