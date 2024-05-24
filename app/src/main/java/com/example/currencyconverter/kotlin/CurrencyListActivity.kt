459e1bf4f0f819787731ae05cea9213b9d664061
Goose-py <slama.yassine3@gmail.com>
2024-05-25 00:01:22 +0200

    Clicking now in the list leads to the map location of the capital

--- app/src/main/java/com/example/currencyconverter/kotlin/CurrencyListActivity.kt
+++ app/src/main/java/com/example/currencyconverter/kotlin/CurrencyListActivity.kt
@@ -1,10 +1,13 @@
 package com.example.currencyconverter.kotlin
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.AdapterView
 import android.widget.ListView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.example.currencyconverter.R
@@ -26,7 +29,18 @@ class CurrencyListActivity : AppCompatActivity() {
         val adapter = CurrencyListAdapter(this, data.toList())
         listView.adapter = adapter
 
-
+        listView.onItemClickListener =
+            AdapterView.OnItemClickListener { _, _, position, _ ->
+                val selectedEntry = adapter.getItem(position)
+                val mapURI = exchangeRateDatabaseObj.getCapital(selectedEntry.toString())
+                println(mapURI)
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=${exchangeRateDatabaseObj.getCapital(selectedEntry.toString())}"))
+                if(intent.resolveActivity(packageManager)!=null){
+                    startActivity(intent)
+                }else{
+                    Toast.makeText(applicationContext, "No application can handle this request.", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -38,7 +52,7 @@ class CurrencyListActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId){
             R.id.currListItem ->{
-                val intent : Intent = Intent(this, MainActivity::class.java)
+                val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
                 true
             }else->onOptionsItemSelected(item)
