b8c321bf924ed015b44088d78aacf94c97ae219e
Goose-py <slama.yassine3@gmail.com>
2024-05-26 10:30:36 +0200

    Added okhttp to project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
+    implementation(libs.okhttp)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
